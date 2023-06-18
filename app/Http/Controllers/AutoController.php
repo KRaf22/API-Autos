a16a88d421ff3e12f767e58d6b0359db1fc1a37d
KRaf22 <rafagonzalezknappe@gmail.com>
2023-06-18 16:32:25 -0300

    Operaciones CRUD finalizadas en AutoController

--- app/Http/Controllers/AutoController.php
+++ app/Http/Controllers/AutoController.php
@@ -7,7 +7,53 @@ use App\Models\Auto;
 
 class AutoController extends Controller
 {
-    public function Listar(Request $request){
+    public function ListarTodos(Request $request){
         return Auto::all();
+    }
+
+    public function ListarUno(Request $request, $idAuto){
+        return Auto::findOrFail($idAuto);
+    }
+
+    public function EliminarUno(Request $request, $idAuto){
+        $auto = Auto::findOrFail($idAuto);
+        $auto -> delete();
+
+        return ["mensaje" => "El auto con el id $idAuto ha sido eliminado correctamente"];
+
+    }
+
+    public function Insertar(Request $request) {
+        $auto = new Auto;
+
+        $auto -> marca = $request -> post ('marca');
+        $auto -> modelo = $request -> post ('modelo');
+        $auto -> color = $request -> post ('color');
+        $auto -> puertas = $request -> post ('puertas');
+        $auto -> cilindrado = $request -> post ('cilindrado');
+        $auto -> automatico = $request -> post ('automatico');
+        $auto -> electrico = $request -> post ('electrico');
+
+        $auto -> save();
+
+        return $auto;
+
+    }
+
+    public function ModificarUno(Request $request, $idAuto) {
+        $auto = Auto::FindOrFail($idAuto);
+
+        $auto -> marca = $request -> post ('marca');
+        $auto -> modelo = $request -> post ('modelo');
+        $auto -> color = $request -> post ('color');
+        $auto -> puertas = $request -> post ('puertas');
+        $auto -> cilindrado = $request -> post ('cilindrado');
+        $auto -> automatico = $request -> post ('automatico');
+        $auto -> electrico = $request -> post ('electrico');
+
+        $auto -> save();
+
+        return $auto;
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get("/autos",[AutoController::class,"Listar"]);
+Route::get("/autos",[AutoController::class,"ListarTodos"]);
