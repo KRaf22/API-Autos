146527bfbc5c2857b39408194c29a3d96dcc104c
KRaf22 <rafagonzalezknappe@gmail.com>
2023-06-18 17:25:46 -0300

    Agregué todas las rutas de la API

--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 Route::get("/autos",[AutoController::class,"ListarTodos"]);
+
+Route::get("/autos/{d}",[AutoController::class,"ListarUno"]);
+
+Route::delete("/autos/{d}",[AutoController::class,"EliminarUno"]);
+
+Route::post("/autos",[AutoController::class,"Insertar"]);
+
+Route::post("/autos/{d}",[AutoController::class,"ModificarUno"]);
